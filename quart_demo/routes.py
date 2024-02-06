b0af85ea0247505b518b6b4f9be2c494408bc4da
klejdis jorgji <klejdis.jorgji@y42.com>
2024-02-06 17:53:19 +0100

    chore: crud for posts

--- quart_demo/routes.py
+++ quart_demo/routes.py
@@ -2,8 +2,8 @@ from typing import Any
 
 from pydantic.main import BaseModel
 from quart import Blueprint
-from quart_schema import validate_request
-from sqlalchemy import insert
+from quart_schema import validate_request, validate_response
+from sqlalchemy import Result, delete, insert, select
 
 from quart_demo.database.connection import async_session
 from quart_demo.models.models import Posts
@@ -16,19 +16,40 @@ class CreatedResponse(BaseModel):
     id: Any | None = None
 
 
+class DeletedResponse(BaseModel):
+    success: bool
+    rowcount: int
+
+
 @bp.route("/hello")
 async def hello() -> str:
     return "Hello, World!"
 
 
+class PostsResponse(BaseModel):
+    class Post(BaseModel):
+        id: int
+        name: str
+        description: str
+
+    posts: list[Post | None]
+
+
 @bp.get("/posts")
-async def posts() -> str:
-    return "Posts"
+@validate_response(PostsResponse)
+async def posts() -> PostsResponse:
+    async with async_session.begin() as session:
+        result = await session.execute(select(Posts))
+        rez = result.scalars().all()
+        return PostsResponse(posts=[{"id": row.id, "name": row.name, "description": row.description} for row in rez])
 
 
 @bp.get("/posts/<int:post_id>")
-async def post(post_id: int) -> str:
-    return f"Post {post_id}"
+async def post(post_id: int) -> dict[str, Any]:
+    async with async_session.begin() as session:
+        result = await session.execute(select(Posts).where(Posts.id == post_id))
+        rez = result.scalars().first()
+        return {"id": rez.id, "name": rez.name, "description": rez.description}
 
 
 class CreatePostRequest(BaseModel):
@@ -41,10 +62,14 @@ class CreatePostRequest(BaseModel):
 async def create_post(data: CreatePostRequest) -> CreatedResponse:
     async with async_session.begin() as session:
         stmt = insert(Posts).values(name=data.name, description=data.description)
-        result = await session.execute(stmt)
-    return CreatedResponse(success=True, id=result.lastrowid)
+        result: Result[Any] = await session.execute(stmt)
+    return CreatedResponse(success=True, id=result.lastrowid)  # type: ignore
 
 
 @bp.delete("/posts/<int:post_id>")
-async def delete_post(post_id: int) -> str:
-    return f"Post {post_id} deleted"
+async def delete_post(post_id: int) -> DeletedResponse:
+    async with async_session.begin() as session:
+        stmt = delete(Posts).where(Posts.id == post_id)
+        result = await session.execute(stmt)
+
+    return DeletedResponse(success=True, rowcount=result.rowcount)
--- quart_demo/test/test_post_endpoint.py
+++ quart_demo/test/test_post_endpoint.py
@@ -2,13 +2,60 @@ import pytest
 from quart.testing import QuartClient
 
 
-@pytest.mark.asyncio
 async def test_post_create(client: QuartClient, refresh_database: None) -> None:
     response = await client.post(
         "/quart-demo/posts",
         json={"name": "Test Title", "description": "Test Content"},
     )
 
-    await response.get_data(as_text=True)
+    assert response.status_code == 200
+
+    assert await response.json == {"success": True, "id": 1}
+
+
+async def test_post_read(client: QuartClient, refresh_database: None) -> None:
+    # create a post
+    await client.post(
+        "/quart-demo/posts",
+        json={"name": "Test Title", "description": "Test Content"},
+    )
+
+    response = await client.get("/quart-demo/posts")
 
     assert response.status_code == 200
+
+    data = await response.json
+
+    assert data == {"posts": [{"id": 1, "name": "Test Title", "description": "Test Content"}]}
+
+
+async def test_post_read_one(client: QuartClient, refresh_database: None) -> None:
+    # create a post
+    await client.post(
+        "/quart-demo/posts",
+        json={"name": "Test Title", "description": "Test Content"},
+    )
+
+    response = await client.get("/quart-demo/posts/1")
+
+    assert response.status_code == 200
+
+    data = await response.json
+
+    assert data == {"id": 1, "name": "Test Title", "description": "Test Content"}
+
+
+async def test_post_delete(client: QuartClient, refresh_database: None) -> None:
+    # create a post
+    await client.post(
+        "/quart-demo/posts",
+        json={"name": "Test Title", "description": "Test Content"},
+    )
+
+    response = await client.delete("/quart-demo/posts/1")
+
+    assert response.status_code == 200
+
+    data = await response.json
+
+    assert data == {"success": True, "rowcount": 1}
